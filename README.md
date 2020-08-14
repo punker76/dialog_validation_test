ba9e61817e98b3f56b0c44497e192fdeed89fedb
punker76 <punkerat76@gmail.com>
2020-08-14 08:57:53 +0200

    Update sample to use latest versions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# dialog_validation_test
+# MahApps Powershel Test
+
+```
+MahApps.Metro v2.2.0
+ControlzEx v4.3.2
+IconPacks v4.4.0
+Framework: .Net 4.7
+```
+
+![](2020-08-14_08h53_18.png)
+
+![](2020-08-14_08h53_26.png)
--- Testing_GUI.ps1
+++ Testing_GUI.ps1
@@ -1,7 +1,10 @@
-$main_folder = ""
-[System.Reflection.Assembly]::LoadFrom("$main_folder\Assembly\MahApps.Metro.dll")      
-[System.Reflection.Assembly]::LoadFrom("$main_folder\Assembly\System.Windows.Interactivity.dll") 
+$main_folder = "."
+
 [System.Reflection.Assembly]::LoadWithPartialName('presentationframework') | out-null
+
+foreach ($file in (Get-ChildItem "$main_folder\assembly\" -File *.dll)) {
+    Add-Type -Path $file.PSPath
+}
 
 function LoadXml ($global:filename)
 {
@@ -31,8 +34,20 @@ $Cliquez = $Form.findname("Cliquez")
 # })
 
 $Cliquez.Add_Click({
+	$Settings = [MahApps.Metro.Controls.Dialogs.LoginDialogSettings]::new()
+	# $Settings.ColorScheme              = "Accented"
+    $Settings.DialogMessageFontSize    = "14"
+	$Settings.DialogTitleFontSize      = "16"
+	$Settings.NegativeButtonVisibility = "Visible"
+    $Settings.DialogResultOnCancel     = "Canceled"
+    $Settings.UsernameWatermark        = "Username..."
+    $Settings.PasswordWatermark        = "Password..."
+    $Settings.UsernameCharacterCasing  = "Normal"
+    $Settings.DefaultButtonFocus       = "Affirmative"
+    $Settings.EnablePasswordPreview    = "True"
 	$ok       = [MahApps.Metro.Controls.Dialogs.MessageDialogStyle]::AffirmativeAndNegative
-    $result   = [MahApps.Metro.Controls.Dialogs.DialogManager]::ShowModalMessageExternal($Form, "Test", "...42", $ok)
+    # $result   = [MahApps.Metro.Controls.Dialogs.DialogManager]::ShowModalMessageExternal($Form, "Test", "...42", $ok)
+    $result   = [MahApps.Metro.Controls.Dialogs.DialogManager]::ShowModalLoginExternal($Form, "Test", "...42", $Settings)
 })
 
 $Form.ShowDialog() | Out-Null
